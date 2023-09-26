b68f5e92d8d3f4fcc31e2f2aea8d7a52e31c0e0f
Matthew Steinkamp <mdsteinkamp@gmail.com>
2023-09-26 16:59:12 -0700

    commit tree f2279416290787a719517538080467c43eb7b2c4

--- lib/balancing_parentheses.rb
+++ lib/balancing_parentheses.rb
@@ -1,3 +1,18 @@
 require_relative './stack'
 
-# your code here
+def balancing_parentheses(input)
+  my_stack = Stack.new
+  input.chars.each do |c|
+
+    if my_stack.peek == "(" && c == ")"
+      my_stack.pop
+    else my_stack.push(c)
+    end
+  end
+  my_stack.size
+end
+
+p balancing_parentheses("()")
+p balancing_parentheses(')()()')
+p balancing_parentheses('(()()(()')
+p balancing_parentheses('))))((((')
